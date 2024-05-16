1bcffa62ad57a3d29b70d446dc95589b7f91d1b1
Jesper Højlund <github@hillgrove.dk>
2024-05-16 18:07:31 +0200

    Image upload working

--- GC_Subscription/Models/Product.cs
+++ GC_Subscription/Models/Product.cs
@@ -14,14 +14,14 @@ namespace GC_Subscription.Models
         [Required(ErrorMessage ="Angiv venligst en beskrivelse af retten")]
         public string Description { get; set; }
 
-        [Range(0, 1000, ErrorMessage ="Prisen skal være mellem 0 og 1000")]
+        [Required(ErrorMessage ="Angiv venligst et beløb")]
+        [Range(1, 1000, ErrorMessage = "Prisen skal være mellem 1 og 1000")]
         public int Price { get; set; }
 
         public bool InStock { get; set; } = true;
         
-        // TODO: Fixing Image Uploading
-        // Adding this back, crashes the app on Product creation. Why? Sunrays or Lucifer... I dunno
-        //public string ImageUrl { get; set; } 
+        // TODO: Adding this back, crashes the app on Product creation. Why? Sunrays or Lucifer... I dunno
+        public string? ImageUrl { get; set; } 
 
         // Relational Fields
         public ICollection<Mealbox> Mealboxes { get; } = [];
