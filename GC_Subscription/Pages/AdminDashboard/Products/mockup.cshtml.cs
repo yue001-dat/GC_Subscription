30bd01470ad93431f49cd1ad98f0402ec2690c9f
Salah Eddin Salam <144003465+SalahSalam@users.noreply.github.com>
2024-05-21 16:31:10 +0200

    Products mockup

--- GC_Subscription/Pages/AdminDashboard/Products/mockup.cshtml.cs
+++ GC_Subscription/Pages/AdminDashboard/Products/mockup.cshtml.cs
@@ -1,12 +1,25 @@
+using GC_Subscription.Data;
+using GC_Subscription.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 
 namespace GC_Subscription.Pages.AdminDashboard.Products
 {
     public class MockupModel : PageModel
     {
-        public void OnGet()
+        private readonly GhostchefContext _context;
+        public IList<Product> Product { get; set; } = default!;
+        public MockupModel(GhostchefContext context)
         {
+            _context = context;
+        }
+        public async Task OnGetAsync()
+        {
+            Product = await _context.Product
+                                    .Include(p => p.Allergies)
+                                    .Include(p => p.Diets)
+                                    .ToListAsync();
         }
     }
 }
