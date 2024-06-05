432bf8fb9f036cce5119b313d9d6b681f3e817b2
Jesper Højlund <github@hillgrove.dk>
2024-06-05 21:11:25 +0200

    Added buy index and integrated buy-predefined

---
+++ GC_Subscription/Pages/Test.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace GC_Subscription.Pages
+{
+    public class TestModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
